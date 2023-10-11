fbdf4d74a625a03e9373249f578161a6043e8d41
Vladimir <shues@bk.ru>
2023-10-11 12:52:18 +0300

    fin

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+//   entry: './src/index.js', // Точка входа для сборки проекта
+
+//   output: {
+    // filename: 'bundle.js', // Имя выходного файла сборки
+    // path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+//   },
+
+module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    open: true, // Автоматически открывать браузер
+  },
+
+  mode: 'development', // Режим сборки
+};
